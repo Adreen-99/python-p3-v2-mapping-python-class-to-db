cf1f12078160b24e2f6234eb5dff656b31e20bc4
+ <+githinjiadreen27@gmail.com>
2025-08-21 15:51:57 +0300

    Testing code

--- lib/debug.py
+++ lib/debug.py
@@ -4,6 +4,31 @@
 from __init__ import CONN, CURSOR
 from department import Department
 
-import ipdb
+# import ipdb
+
+Department.drop_table()
+Department.create_table()
+
+payroll = Department("Payroll", "Building A, 5th Floor")
+print(payroll)  
+
+payroll.save()  
+print(payroll)  
+
+hr = Department("Human Resources", "Building C, East Wing")
+print(hr)  
+
+hr.save()  
+print(hr) 
+
+hr.name = 'HR'
+hr.location = "Building F, 10th Floor"
+hr.update()
+print(hr) 
+
+print("Delete Payroll")
+payroll.delete()  
+print(payroll) 
+
 
 ipdb.set_trace()
